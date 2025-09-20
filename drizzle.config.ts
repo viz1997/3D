f498c91eeddaf137c7dc36e29fadf20a09aa8494
weijunext <weijunext@gmail.com>
2025-09-20 22:55:50 +0800

    chore: load environment variables in drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,9 @@
+import { loadEnvConfig } from '@next/env';
 import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
+
+const projectDir = process.cwd();
+loadEnvConfig(projectDir);
 
 export default defineConfig({
   out: './lib/db/migrations',
